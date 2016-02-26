FIX: density normalization now returns sparse matrices.
- also symmetrizing the kernel matrix is now in terms of sparse matrices

diffusionMaps.py
@@ -1,7 +1,7 @@
 from sklearn.base import BaseEstimator
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-from scipy.sparse import csr_matrix, issparse
+from scipy.sparse import csr_matrix, issparse, diags
 from scipy.sparse.linalg import svds, eigsh
 import matplotlib.pyplot as plt
 
@@ -53,6 +53,9 @@ class DiffusionMap(BaseEstimator):
                 print('density normalization')
             kernelMat = self.__density_normalize__(kernelMat)
 
+        #also, store the kernel matrix (mostly debugging)
+        self.kernelMat = kernelMat
+
         # calculate the eigenvectors of the matrx
         if self.verbose:
             print("Calculating eigenvalue decomposition")
@@ -62,8 +65,6 @@ class DiffusionMap(BaseEstimator):
         # eigsh returns the k largest eigenvals but ascending order (smallest first), so resort
         ix = lambdas.argsort()[::-1]
 
-        #also, store the kernel matrix (mostly debugging)
-        self.kernelMat = kernelMat
         return V[:,ix], lambdas[ix]
 
     def __density_normalize__(self, kernelMat):
@@ -75,15 +76,21 @@ class DiffusionMap(BaseEstimator):
         """
         assert issparse(kernelMat), 'K must be sparse, multiplication behaves very differnently for sparse/dense (elementwise vs mat-mult)'
 
-        # Z(x), kind of the partition function
-        Z = kernelMat.sum(0)   # WARNING Z is of type matrix
+        "calculate:  P_xy / Z(x)Z(y)"
+        # rescale each column by Z and also each row by Z
+        # easily done by just multipling with a diagonal matrix from the left (scaling rows) and right (rescaling columsn)
+        # not that row and column sum are the same as the matrix is symmetric!!
 
-        # here we now pretend/define that x runs along the columns of the matrix (x is the column index)
-        P_tilde = kernelMat / np.dot(Z.reshape(-1,1), Z.reshape(1,-1))  # P_xy / Z(x)Y(y)
+        # Z(x), kind of the partition function
+        Z = np.array(kernelMat.sum(0)).flatten()  # a bit ugly, Z is this strange type(matrix), which one cannot cast into a 1d array, hence the detour to np.array
+        scalingMat = diags(1.0 / Z)  # multiplying by this (one the right) is equivalent of dividing each row by Z
+        P_tilde = scalingMat * kernelMat * scalingMat  # this is matrix multiply!
 
         # Eq (5,6) of [1]
-        Z_tilde = P_tilde.sum(0)
-        P_tilde /= Z_tilde
+        # once again, the same trick with diagonal matrix for resacling
+        Z_tilde = np.array(P_tilde.sum(0)).flatten()
+        scalingMat = diags(1.0 / Z_tilde)
+        P_tilde = P_tilde * scalingMat
 
         return P_tilde
 
@@ -108,11 +115,21 @@ class DiffusionMap(BaseEstimator):
         return distances, indices
 
     def __get_kernel_matrix__(self, X, k):
+        """
+        returns the kernel matrix for the samples in X using a Gaussian Kernel and a kNN-approximation,
+
+        - all distances are zero, except within the neartest neighbours
+        - also symmetrizing the matrix (kNN is not symmetric necceseraly)
+
+        :param X: data matrix NxF, where N=number of samples, F= number of features
+        :param k: number of nearest neighbours to consider in kNN
+        :return: symmetric sparse matrix of NxN
+        """
+
         distances, indices = self.__get_NN__(X, k=k)
         diffDist = np.exp(-(0.5/self.sigma**2) * distances**2)
 
-        # build a sparse matrix out of the diffusionDistances
-        # some crazy magic with the sparse matrixes
+        # build a sparse matrix out of the diffusionDistances; some crazy magic with the sparse matrixes
         N = X.shape[0]
         indptr = range(0, (N+1)*k, k)   # some helper matrix, specfiing that the first k indices in indices,flatten() belong to the first row of data
 
@@ -122,11 +139,10 @@ class DiffusionMap(BaseEstimator):
         # (if x is a kNN of y, y doesnt have to be a kNN of x)
         # lets make it symmetric again, just filling in the missing entries
 
-        # TODO K + K.T*(K==0) is inefficient due to the K==0 where K is sparse
-        assert issparse(K), 'K must be sparse, multiplication behaves very differnently for sparse/dense (elementwise vs mat-mult)'
-        K = K + K.T.multiply((K==0)) # WARNING: for sparse matrices, '*' is overloaded as matrix multiplication!!
+        shared_mask = (K!=0).multiply(K.T!=0)  # marking entries that are nonzero in both matrixes. mulitply is elemntwise!
+        K_sym = K + K.T - K.multiply(shared_mask) # adding them up, subtracting the common part that was counted twice!
 
-        return K
+        return K_sym
 
 
 if __name__ == '__main__':