5f6bccb9a30b27ef1464fc90a1e68d09e49213a9
redst4r <redst4r@web.de>
2017-03-24 13:19:59 +0100

    refactoring

--- diffusionMaps.py
+++ diffusionMaps.py
@@ -2,13 +2,81 @@ from sklearn.base import BaseEstimator
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 from scipy.sparse import csr_matrix, issparse, diags
-from scipy.sparse.linalg import svds, eigsh
+from scipy.sparse.linalg import eigsh
+from scipy.linalg import eigh
 import matplotlib.pyplot as plt
+import scipy.spatial.distance
+
+
+def _check_Z_for_division(Z,eps):
+    "Z might be zero sometimes, we add a small constant epsilon to it. make sure this doesnt change to much"
+
+    # check that all nonzeros of z are significantly larger than eps
+    ixNonZero = Z!=0
+    assert np.all(np.abs(Z[ixNonZero]) > eps*10), 'values to small. might introduce some error since close to zero division'
+
+
+def _density_normalize(kernelMat):
+    """
+    density normalization: Eq (4-5) of [1]
+
+    be very careful here if K is a sparse matrix, which behaves differently from usual np.ndarray
+    in terms of operators *, /
+    """
+    eps = 1e-100
+
+    # the method only works on symmetric matrices (relies that Z is the same along rows and cols)
+    atol_symmetric = 1e-10  #TODO loose tolerance
+    if issparse(kernelMat):
+        np.testing.assert_allclose((kernelMat-kernelMat.T).A, 0, atol=atol_symmetric)
+    else:
+        np.testing.assert_allclose(kernelMat-kernelMat.T, 0, atol=atol_symmetric)
+
+    "calculate:  P_xy / Z(x)Z(y)"
+    # rescale each column by Z and also each row by Z
+    # easily done by just multipling with a diagonal matrix from the left (scaling rows) and right (rescaling columsn)
+    # note that row and column sum are the same as the matrix is symmetric!!
+    # Z(x), kind of the partition function
+    if issparse(kernelMat):
+        Z = np.array(kernelMat.sum(0)).flatten()  # a bit ugly, Z is this strange type(matrix), which one cannot cast into a 1d array, hence the detour to np.array
+        _check_Z_for_division(Z, eps)
+
+        scalingMat = diags(1.0 / (Z + eps), offsets=0)  # multiplying by this (one the right) is equivalent to rescaling the columns
+        P_tilde = scalingMat * kernelMat * scalingMat  # this is matrix multiply!
+    else:
+        Z = kernelMat.sum(0).flatten() # make sure it doesnt have two dimensions, needed for the broadcasting below
+        _check_Z_for_division(Z, eps)
+
+        invZ = 1.0 / (Z + eps)  # careful about zero division.
+
+        #TODO replace by matrix multiplicaition?!  ->  M@N
+        P_tilde = kernelMat * invZ * invZ.reshape(-1,1) # broadcasts along rows and columsn, sclaing them both
+
+
+    # Eq (5,6) of [1]
+    # once again, the same trick with diagonal matrix for resacling
+    if issparse(kernelMat):
+        Z_tilde = np.array(P_tilde.sum(0)).flatten()
+        _check_Z_for_division(Z_tilde, eps)
+        scalingMat = diags(1.0 / (Z_tilde + eps), offsets=0)
+        P_tilde = P_tilde * scalingMat
+    else:
+        Z_tilde = P_tilde.sum(0).flatten() # make sure it doesnt have two dimensions, needed for the broadcasting below
+        _check_Z_for_division(Z_tilde, eps)
+
+        invZ_tilde = 1.0 / (Z_tilde + eps)
+
+        #same fuzz about the zero
+        ixnonZero = Z_tilde != 0
+        P_tilde[np.ix_(ixnonZero, ixnonZero)] = P_tilde[np.ix_(ixnonZero, ixnonZero)] * invZ_tilde[ixnonZero] #normalizes each column
+
+    return P_tilde
 
 class DiffusionMap(BaseEstimator):
 
     """
     diffusion maps for dimension reduction along the lines of [1]
+    this one uses nearest neighbours to approximate the kernel matrix
 
     [1] Haghverdi, L., Buettner, F. & Theis, F. J.
         Diffusion maps for high-dimensional single-cell analysis of differentiation data.
@@ -41,7 +109,7 @@ class DiffusionMap(BaseEstimator):
         k = min(self.k, X.shape[0])  # limit the number of neighbours
         if self.verbose:
             print("Calculating kernel matrix")
-        kernelMat = self.__get_kernel_matrix__(X, k)
+        kernelMat = self._get_kernel_matrix(X, k)
         if self.verbose:
             print("DONE: Calculating kernel matrix")
 
@@ -51,7 +119,7 @@ class DiffusionMap(BaseEstimator):
         if density_normalize:
             if self.verbose:
                 print('density normalization')
-            kernelMat = self.__density_normalize__(kernelMat)
+            kernelMat = _density_normalize(kernelMat)
 
         #also, store the kernel matrix (mostly debugging)
         self.kernelMat = kernelMat
@@ -60,6 +128,7 @@ class DiffusionMap(BaseEstimator):
         if self.verbose:
             print("Calculating eigenvalue decomposition")
 
+        #TODO Warning: kernel matrix os not symmetric after density normalization, eigsh might fail!?
         lambdas, V = eigsh(kernelMat, k=self.embedding_dim)  # calculate as many eigs as the requested embedding dim
 
         # eigsh returns the k largest eigenvals but ascending order (smallest first), so resort
@@ -67,34 +136,7 @@ class DiffusionMap(BaseEstimator):
 
         return V[:,ix], lambdas[ix]
 
-    def __density_normalize__(self, kernelMat):
-        """
-        density normalization: Eq (4-5) of [1]
-
-        be very careful here. K is a sparse matrix, which behaves differently from usual np.ndarray
-        in terms of operators *, /
-        """
-        assert issparse(kernelMat), 'K must be sparse, multiplication behaves very differnently for sparse/dense (elementwise vs mat-mult)'
-
-        "calculate:  P_xy / Z(x)Z(y)"
-        # rescale each column by Z and also each row by Z
-        # easily done by just multipling with a diagonal matrix from the left (scaling rows) and right (rescaling columsn)
-        # not that row and column sum are the same as the matrix is symmetric!!
-
-        # Z(x), kind of the partition function
-        Z = np.array(kernelMat.sum(0)).flatten()  # a bit ugly, Z is this strange type(matrix), which one cannot cast into a 1d array, hence the detour to np.array
-        scalingMat = diags(1.0 / Z)  # multiplying by this (one the right) is equivalent of dividing each row by Z
-        P_tilde = scalingMat * kernelMat * scalingMat  # this is matrix multiply!
-
-        # Eq (5,6) of [1]
-        # once again, the same trick with diagonal matrix for resacling
-        Z_tilde = np.array(P_tilde.sum(0)).flatten()
-        scalingMat = diags(1.0 / Z_tilde)
-        P_tilde = P_tilde * scalingMat
-
-        return P_tilde
-
-    def __get_NN__(self, dataMatrix, k):
+    def _get_NN(self, dataMatrix, k):
         """
         caluclates the distance to the k-nearest neighbours, 
         return an array of distances and indices of nearest 
@@ -104,17 +146,11 @@ class DiffusionMap(BaseEstimator):
         :param k: number of nearest nneighbours
         :return: 
         """
-        if self.verbose:
-            print('Calculating nearest neighbours')
-
         nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(dataMatrix)
         distances, indices = nbrs.kneighbors(dataMatrix)
-
-        if self.verbose:
-            print('DONE: Calculating nearest neighbours')
         return distances, indices
 
-    def __get_kernel_matrix__(self, X, k):
+    def _get_kernel_matrix(self, X, k):
         """
         returns the kernel matrix for the samples in X using a Gaussian Kernel and a kNN-approximation,
 
@@ -126,7 +162,7 @@ class DiffusionMap(BaseEstimator):
         :return: symmetric sparse matrix of NxN
         """
 
-        distances, indices = self.__get_NN__(X, k=k)
+        distances, indices = self._get_NN(X, k=k)
         diffDist = np.exp(-(0.5/self.sigma**2) * distances**2)
 
         # build a sparse matrix out of the diffusionDistances; some crazy magic with the sparse matrixes
@@ -141,6 +177,8 @@ class DiffusionMap(BaseEstimator):
 
         shared_mask = (K!=0).multiply(K.T!=0)  # marking entries that are nonzero in both matrixes. mulitply is elemntwise!
         K_sym = K + K.T - K.multiply(shared_mask) # adding them up, subtracting the common part that was counted twice!
+
+        np.testing.assert_allclose((K_sym-K_sym.T).A, 0, atol=1e-10)  # todo loose tolerance
 
         return K_sym
 
--- test_DiffusionMap.py
+++ test_DiffusionMap.py
@@ -1,8 +1,9 @@
 import pytest
 import numpy as np
-from diffusionMaps import DiffusionMap
+from diffusionMaps import DiffusionMap, _density_normalize
 from scipy.sparse import csr_matrix, issparse
 import scipy.sparse
+
 
 @pytest.fixture
 def mixtureNormals(request):
@@ -18,6 +19,20 @@ def mixtureNormals(request):
 
     X = np.vstack([X1,X2])
     return X
+
+def create_sym_matrix(n):
+    q = np.random.rand(n,n)
+    return np.dot(q,q.T)
+
+
+def create_sparse_sym_matrix(n, density=0.1): # sparseness = .9 -> 90% entries zero
+    q = np.random.rand(n,n)
+    sym = np.dot(q,q.T)
+    ix = np.random.binomial(1,density/2, size=(n,n))
+    ix = ix+ix.T*(ix==0)  # make the selection of entrys also symmetric
+    return csr_matrix(sym*ix)
+
+
 
 
 def test_DiffusionMap_fit_transform_output_dimensions(mixtureNormals):
@@ -37,7 +52,7 @@ def test_DiffusionMap_nearestNeighbour_number_of_neighbours(mixtureNormals):
     df = DiffusionMap(sigma=1, embedding_dim=embDim)
 
     kNN = 4
-    distances, indices = df.__get_NN__(X,k=kNN)
+    distances, indices = df._get_NN(X,k=kNN)
 
     assert distances.shape == (X.shape[0], kNN)
     assert indices.shape == (X.shape[0], kNN)
@@ -51,7 +66,7 @@ def test_DiffusionMap_get_kernel_matrix_number_of_neighbours(mixtureNormals):
     df = DiffusionMap(sigma=1, embedding_dim=embDim)
 
     kNN = 4
-    K = df.__get_kernel_matrix__(X,k=kNN)
+    K = df._get_kernel_matrix(X,k=kNN)
     assert K.shape == (X.shape[0], X.shape[0])
 
     nonzero_elements_per_row = np.sum(K.toarray()!=0, 1)
@@ -63,7 +78,7 @@ def test_DiffusionMap_get_kernel_matrix_symmetry(mixtureNormals):
     "make sure the kernel matrix is symmetric"
     X = mixtureNormals
     df = DiffusionMap(sigma=1,embedding_dim=2,verbose=False)
-    K = df.__get_kernel_matrix__(X,k=2)
+    K = df._get_kernel_matrix(X,k=2)
 
     Q = (K-K.T).toarray()  # np.all doesnt work on sparse matrices
     assert np.all(Q==0), 'returned kernel matrix is not symmetric'
@@ -71,23 +86,48 @@ def test_DiffusionMap_get_kernel_matrix_symmetry(mixtureNormals):
 
 def test__get_kernel_matrix_sparse(mixtureNormals):
     df = DiffusionMap(sigma=1,embedding_dim=2,verbose=False)
-    K = df.__get_kernel_matrix__(mixtureNormals,k=10)
+    K = df._get_kernel_matrix(mixtureNormals,k=10)
     assert issparse(K)
 
 
 def test__density_normalize__sparse(mixtureNormals):
+    "must return sparse if we put in sparse"
+    K = csr_matrix([[0,1],[1,1]])
+    assert issparse(_density_normalize(K)), 'returned matrix is not sparse after normalization'
 
-    df = DiffusionMap(sigma=1,embedding_dim=2,verbose=False)
-    K = csr_matrix([[1,1],[0,1]])
-    assert issparse(df.__density_normalize__(K)), 'returned matrix is not sparse after normalization'
 
 def test__density_normalize__colsum(mixtureNormals):
-
-    df = DiffusionMap(sigma=1,embedding_dim=2,verbose=False)
-    K = scipy.sparse.rand(100,100,density=0.1)
-    K_norm = df.__density_normalize__(K)
+    K = create_sparse_sym_matrix(100, density=0.1)
+    K_norm = _density_normalize(K)
 
     np.testing.assert_allclose(K_norm.toarray().sum(0), 1)
+
+
+def test__test__density_normalize__not_sparse(mixtureNormals):
+    K = create_sym_matrix(2)
+    K_norm = _density_normalize(K)
+
+    assert isinstance(K_norm, np.ndarray), 'must return full matrix if we put in a full matrix'
+
+
+def test__density_normalize__not_sparse_colsum(mixtureNormals):
+    K = create_sym_matrix(100)
+    K_norm = _density_normalize(K)
+
+    np.testing.assert_allclose(K_norm.sum(0), 1)
+
+
+def test_density_normalize_same_result_sparse_nonsparse():
+
+    for d in [0.0001, 0.01, 0.5]:  # test it for different sparsity, as sometimes entre rows/col become zero
+        K_sparse = create_sparse_sym_matrix(5, density=d)
+        K_full = K_sparse.A
+
+        n_sparse = _density_normalize(K_sparse).A
+        n_full = _density_normalize(K_full)
+
+        np.testing.assert_allclose(n_sparse, n_full)
+
 
 def test_DiffusionMap_fit_transform_eigenvalue_ordering(mixtureNormals):
     "must return the largest first"
@@ -97,16 +137,3 @@ def test_DiffusionMap_fit_transform_eigenvalue_ordering(mixtureNormals):
     df = DiffusionMap(sigma=1, embedding_dim=embDim )
     X_embed, lam = df.fit_transform(X)
     assert(lam[0]> lam[1])
-
-
-def test_DiffusionMap_density_normalization(mixtureNormals):
-
-    df = DiffusionMap(sigma=1, embedding_dim=2 )
-
-    A = np.random.normal(0,1,size=(10,2))
-    K = np.dot(A,A.T)  # 10x10 symmetric kernel-matrix
-    K = csr_matrix(K)
-
-    K_norm = df.__density_normalize__(K)
-    theSum = K_norm.sum(0)
-    np.testing.assert_allclose(theSum, 1)
